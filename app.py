514d4ef44f7a8b193660d7cf45e61ff070e017e4
Himanshu <hs127010@gmail.com>
2022-12-08 15:21:18 +0530

    initial commit

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ books2 = pickle.load(open('books1.pkl','rb'))
 similarity_scores =  pickle.load(open('similarity_scores1.pkl','rb'))
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///the-book-spot.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///the-book-spot.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://mthwnkvmlsvjzp:ec522ca94a36167296640a3c861dc957da3d0ea3a5f41ee1d63dc5bc73807d89@ec2-44-205-177-160.compute-1.amazonaws.com:5432/d19lnnc3bfcl9n"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://mthwnkvmlsvjzp:ec522ca94a36167296640a3c861dc957da3d0ea3a5f41ee1d63dc5bc73807d89@ec2-44-205-177-160.compute-1.amazonaws.com:5432/d19lnnc3bfcl9n"
 db = SQLAlchemy(app)
 
 
